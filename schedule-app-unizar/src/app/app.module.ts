db6825bbf5b258114a1bc5a64f4ac15177fc597e
iaydemirankabeta <theangelo112@gmail.com>
2024-05-08 16:17:57 +0200

    Map added using OpenLayers

--- schedule-app-unizar/src/app/app.module.ts
+++ schedule-app-unizar/src/app/app.module.ts
@@ -8,7 +8,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InputFormComponent } from './components/input-form/input-form.component';
 import { InteractiveMapComponent } from './components/interactive-map/interactive-map.component';
-import { UserInfoComponent } from './components/user-info/user-info.component';
 import { SpaceInfoComponent } from './components/space-info/space-info.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 import { FormsModule } from '@angular/forms';
