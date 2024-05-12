89d8b5f5f9515ffd0bb5aac7ae83d812cf9d950e
iaydemirankabeta <theangelo112@gmail.com>
2024-05-12 18:48:41 +0200

    Update make-reservation.component.ts

--- schedule-app-unizar/src/app/components/espacios/make-reservation/make-reservation.component.ts
+++ schedule-app-unizar/src/app/components/espacios/make-reservation/make-reservation.component.ts
@@ -11,38 +11,38 @@ import { User } from '../../../models/user';
   styleUrls: ['./make-reservation.component.scss'],
 })
 export class MakeReservationComponent {
-  user!: User;
-  reservationData: Reserva = {
-    id: '',
-    person: {
-      nombre: '',
-      email: '',
-      roles: [],
-      departamento: '',
-    },
-    espacio: {
-      id: this.data.id,
-      tamano: this.data.tamano,
-      categoriaReserva: this.data.categoriaReserva,
-      numMaxOcupantes: this.data.capacidadMaxima,
-      planta: 0,
-      reservable: this.data.reservable,
-    },
-    infoReserva: {
-      numMaxPersonas: 0,
-      fechaInicio: '',
-      fechaFinal: '',
-      descripcion: '',
-      tipoUsoReserva: '',
-    },
-  };
+  reservationData: Reserva;
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
     private authService: AuthService,
     private reservaService: ReservaService
   ) {
-    this.user.email = this.authService.getLoggedInPersonEmail();
+    const defaultUser = this.authService.getLoggedPersonInfo();
+    this.reservationData = {
+      id: defaultUser.email,
+      person: {
+        nombre: defaultUser.nombre,
+        email: defaultUser.email,
+        roles: defaultUser.roles,
+        departamento: defaultUser.departamento,
+      },
+      espacio: {
+        id: this.data.id,
+        tamano: this.data.tamano,
+        categoriaReserva: this.data.categoriaReserva,
+        numMaxOcupantes: this.data.capacidadMaxima,
+        planta: 0,
+        reservable: this.data.reservable,
+      },
+      infoReserva: {
+        numMaxPersonas: 0,
+        fechaInicio: '',
+        fechaFinal: '',
+        descripcion: '',
+        tipoUsoReserva: '',
+      },
+    };
   }
 
   reserve(): void {
