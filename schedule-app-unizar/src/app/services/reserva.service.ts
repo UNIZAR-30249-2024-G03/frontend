b60821074b5e61483809d6b346eaa459d24a69f9
iaydemirankabeta <theangelo112@gmail.com>
2024-05-15 13:38:24 +0200

    fix reserva post method.

--- schedule-app-unizar/src/app/services/reserva.service.ts
+++ schedule-app-unizar/src/app/services/reserva.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Reserva } from '../models/reserva';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root',
@@ -9,7 +10,7 @@ import { Reserva } from '../models/reserva';
 export class ReservaService {
   apiUrl = '/api';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private authService: AuthService) {}
 
   getReservas(email: string): Observable<Reserva[]> {
     return this.http.get<Reserva[]>(
@@ -17,15 +18,28 @@ export class ReservaService {
     );
   }
 
-  addReserva(reservationData: Reserva): Observable<Reserva> {
-    const idUsuario = reservationData.person.email;
-
-    // idUsuario parametresini URL'ye eklemek için HttpParams kullanalım
-    let params = new HttpParams();
-    params = params.set('idUsuario', idUsuario);
+  addReserva(reservationData: Reserva): Observable<any> {
+    const idUsuario = this.authService.getLoggedInPersonEmail();
 
     // POST isteği için gerekli verileri gönderelim
-    return this.http.post<any>(this.apiUrl, reservationData, { params });
+    return this.http.post<any>(
+      this.apiUrl +
+        '/reservas?idUsuario=' +
+        idUsuario +
+        '&idsEspacios=' +
+        reservationData.espacio.id +
+        '&tipoUsoReserva=' +
+        reservationData.infoReserva.tipoUsoReserva +
+        '&numMaxOcupantes=' +
+        reservationData.infoReserva.numMaxPersonas +
+        '&fechaInicio=' +
+        reservationData.infoReserva.fechaInicio +
+        '&fechaFinal=' +
+        reservationData.infoReserva.fechaFinal +
+        '&descripcion=' +
+        reservationData.infoReserva.descripcion,
+      reservationData
+    );
   }
 
   updateReserva(id: string, reservationData: Reserva): Observable<Reserva> {
